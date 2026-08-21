3a090fe7b9b78ae8c65b8e4ac77f9a21a855462a
Florryshine <florryshine5@gmail.com>
2026-08-21 02:51:18 +0100

    Add Meta Pixel tracking

--- app/layout.js
+++ app/layout.js
@@ -31,6 +31,31 @@ export default function RootLayout({ children }) {
           src="https://js.paystack.co/v1/inline.js"
           strategy="beforeInteractive"
         />
+        {/* Meta Pixel Code */}
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '2879100209116929');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: 'none' }}
+            src="https://www.facebook.com/tr?id=2879100209116929&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
+        {/* End Meta Pixel Code */}
       </body>
     </html>
   );
